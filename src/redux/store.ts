941deb22b25f578350568b93554d8b0b4ba2309d
Dominik Adrian Grzywak <starterx4@gmail.com>
2025-03-13 04:16:29 +0100

    forced migration to React v19, cleanings

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,11 +2,11 @@ import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { userDataReducer } from './slices/userDataSlice';
 import storage from 'redux-persist/lib/storage';
 import { persistReducer, persistStore } from 'redux-persist';
-import { bindsReducer } from './slices/bindsSlice';
-import { bindSuggestionsReducer } from './slices/bindSuggestionsSlice';
-import { gameStatsReducer } from './slices/gameStatsSlice';
+//import { bindsReducer } from './slices/bindsSlice';
+//import { bindSuggestionsReducer } from './slices/bindSuggestionsSlice';
+//import { gameStatsReducer } from './slices/gameStatsSlice';
 import { serverInfoReducer } from './slices/serverInfoSlice';
-import { bindVotesReducer } from './slices/bindVotesSlice';
+//import { bindVotesReducer } from './slices/bindVotesSlice';
 
 const persistConfig = {
     key: 'root',
@@ -16,11 +16,11 @@ const persistedUserDataReducer = persistReducer(persistConfig, userDataReducer);
 
 const combinedReducers = combineReducers({
     userDataReducer: persistedUserDataReducer,
-    bindsReducer: bindsReducer,
-    bindSuggestionsReducer: bindSuggestionsReducer,
-    gameStatsReducer: gameStatsReducer,
+    // bindsReducer: bindsReducer,
+    // bindSuggestionsReducer: bindSuggestionsReducer,
+    // gameStatsReducer: gameStatsReducer,
     serverInfoReducer: serverInfoReducer,
-    bindVotesReducer: bindVotesReducer,
+    // bindVotesReducer: bindVotesReducer,
 });
 
 export const appStore = configureStore({
