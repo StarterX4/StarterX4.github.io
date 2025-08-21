fec84aa379718f81f5c319f910b5bac7657c5636
Dominik Adrian Grzywak <starterx4@gmail.com>
2025-08-21 21:27:57 +0200

    CLEAN UP unused leftovers from Kether base

--- src/models/adminModels.ts
+++
@@ -1,4 +0,0 @@
-export type Admin = {
-	name: string;
-	steamID: string;
-};
--- src/utils/apiPaths.ts
+++
@@ -1,4 +0,0 @@
-export const apiPaths = {
-    DOMAIN_API: `https://kether-api.click/api`,
-    DOMAIN_LOCAL_API: `https://localhost:7201/api`,
-};
