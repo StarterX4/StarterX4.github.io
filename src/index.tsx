7f810c206d358eb6a1ed6fda7d1fbb8dfe8ab877
Dominik Adrian Grzywak <starterx4@gmail.com>
2025-10-21 06:02:04 +0200

    .

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import 'primereact/resources/themes/bootstrap4-dark-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
 import KetherApplication from './components/KetherAppMain/KetherApplication';
-import { appStore, persistedAppStore } from './redux/store';
 import './global.css';
 import { reportWebVitals } from './utils/webVitals';
 import { preloadCriticalComponents } from './utils/preloading';
